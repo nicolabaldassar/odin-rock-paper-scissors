32c0908156ef0ea2212fb6fb34efaabbe8dc410f
¨Nicola¨ <nicola.baldassar13@gmail.com>
2022-07-17 12:04:02 +0200

    Finisched the main logic part of the game, now you can play on the browser console.

--- javascript.js
+++ javascript.js
@@ -3,11 +3,11 @@ function getComputerChoice (min, max)
 {
     let choice = (Math.floor(Math.random() * max ) + min);
     if(choice == 1)
-        return "rock";
+        return "Rock";
     else if (choice == 2)
-        return "paper";
+        return "Paper";
     else
-        return "scissors";
+        return "Scissors";
 }
 //get the player choice
 function getPlayerChoice()
@@ -22,8 +22,50 @@ function getPlayerChoice()
 //play a single round
 function playRound(playerSelection, computerSelection)
 {
-    computerSelection = getComputerChoice(1, 3);
-    playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    let result;
+    //choose the winner
+    if (computerSelection == "Rock")
+    {
+        if (playerSelection == "rock")
+            result = "Tie!";
+        else if (playerSelection == "paper")
+            result = "You win! Paper beats Rock!"
+        else
+            result = "You lose! Rock beats Scissors!"
+    }
+    else if (computerSelection == "Paper")
+    {
+        if (playerSelection == "rock")
+            result = "You lose! Paper beats Rock!";
+        else if (playerSelection == "paper")
+            result = "Tie!"
+        else
+            result = "You win! Scissors beats Rock!"
+    }
+    else
+    {
+        if (playerSelection == "rock")
+            result = "You win! Rock beats Scissors!";
+        else if (playerSelection == "paper")
+            result = "You lose! Scissors beats Paper!"
+        else
+            result = "Tie!"
+    }
+    return result;
 }
-playRound();
+//this function manage the entire game cycle
+function game()
+{
+    console.log("The game started!");
+    for (let i = 0; i < 5; i++)
+    {
+        let computerSelection = getComputerChoice(1, 3);
+        let playerSelection = getPlayerChoice();
+        console.log("This is a new round!");
+        console.log("The computer selection is " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log("The game is finished!");
+}
+//here I start the game calling the main function
+game();
